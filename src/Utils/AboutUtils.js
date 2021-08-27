be41ae400fa901b34b42bf72c4d78247062b54ad
javivi235 <javisoruco2305@gmail.com>
2021-08-27 15:49:54 -0400

    Footer unit tests added

---
+++ src/Utils/AboutUtils.js
@@ -0,0 +1,19 @@
+function getAuthor(){
+    return
+}
+
+function getPhone(){
+    return
+}
+    
+function getMail(){
+    return
+}
+
+function getLocation(){
+    return
+}
+
+export {getAuthor, getPhone, getMail, getLocation}
+
+
