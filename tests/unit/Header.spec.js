f6c8ca86f9d183484a4f852a528bcfc7c6af3534
javivi235 <javisoruco2305@gmail.com>
2021-08-27 12:30:35 -0400

    modified unit testing headers to include link test

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -1,22 +1,35 @@
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+
 import Header from '@/components/Header.vue'
 
 describe('Header', () => {
 
+    let localVue
+    const router = new VueRouter()
     let wrapper
     const title = 'True Music Index'
 
     beforeEach(() => {
 
+        localVue = createLocalVue()
+        localVue.use(VueRouter)
+
         wrapper = shallowMount(Header, {
-            propsData: { title }
+            propsData: { title },
+            localVue,
+            router
         })
 
     })
 
-    it('Aplication Navbar shoud exist', () => {
+    it('Aplication Navbar shoud exist and shoud redirect to home', () => {
         expect(wrapper.find('#navbar').exists()).toBe(true)
+        link = wrapper.find('router-link')
+        link.trigger('click')
+        expect(wrapper.vm.$route.path).toBe('/')
+    
     })
 
     it('Renders application name', () => {
