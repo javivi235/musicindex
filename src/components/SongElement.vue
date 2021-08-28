ac3d6390219670bbeed89176a5761e255a817de3
javivi235 <javisoruco2305@gmail.com>
2021-08-27 23:46:51 -0400

    SongElement component finished, unit tests removes as they were found not really useful

--- src/components/SongElement.vue
+++ src/components/SongElement.vue
@@ -1,5 +1,31 @@
 <template>
-    <h1>Card</h1>
+
+    <div class="card songCard" style="width: 18rem;">
+
+        <img
+        :src="image"
+        class="card-img-top rounded"
+        :alt="name"
+        >
+
+        <div class="card-body">
+
+            <h5 class="card-title trackName">{{ name }}</h5>
+            <p class="card-text trackInfo">{{ album }} - {{artist}}</p>
+            <p class="card-text trackRelease">{{ time }}</p>
+            <a
+            class="btn buyButton"
+            :href="link"
+            role="button"
+            target="_blank"
+            >
+            {{ price }} 
+            </a>
+
+        </div>
+
+    </div>
+
 </template>
 
 <script>
@@ -16,6 +42,7 @@
             album: String,
             time: String,
             price: String,
+            link: String
 
         },
 
@@ -24,5 +51,32 @@
 </script>
 
 <style scoped>
+
+    .songCard {
+
+        align-items: center;
+        margin: 30px;
+        background-color: #444444;
+        color: white;
+        border-style: solid;
+        border-width: 1px;
+        border-color: black;
+
+    }
+
+    img {
+        border-style: solid;
+        border-width: 1px;
+        margin-top: 20px;
+        border-color: black;
+        width: 98%;
+        height: 60%;
+        
+    }
+
+    .buyButton {
+        background-color: #013220;
+        color: white;
+    }
 
 </style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -5,6 +5,17 @@
             <div class="row">
                <SearchBar @searchSong="searchSong"/>
             </div>
+            <div class="row">
+                <SongElement
+                image = "https://is5-ssl.mzstatic.com/image/thumb/Music114/v4/2b/99/68/2b996877-2fbe-391a-07f1-886df7d304c8/source/100x100bb.jpg"
+                name = "Boss's Daugther"
+                artist = "Pop Evil"
+                album = "War of Angels"
+                time = "2011"
+                price = "7.99 USD"
+                link = "https://music.apple.com/us/album/bosss-daughter/483220906?i=483221313&uo=4"
+                />
+            </div>
         </div>
 
     </div>
@@ -12,6 +23,7 @@
 
 <script>
     import SearchBar from '@/components/SearchBar.vue'
+    import SongElement from '@/components/SongElement.vue'
     import { searchSong } from '../Utils/ITunesAPIUtil.js'
 
     export default {
@@ -43,6 +55,7 @@
         components: {
 
             SearchBar,
+            SongElement,
         
         },
     
